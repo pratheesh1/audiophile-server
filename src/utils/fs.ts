4b086a39e2b65aca4ad6b15660beb27a8305a5d6
Pratheesh Soman <pratheesh1269@gmail.com>
2022-10-27 08:43:57 -0400

    refactor: remove unnecessary log

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,6 +1,6 @@
 /* eslint-disable security/detect-non-literal-fs-filename */
 import { isDevEnv } from "@utils/config";
-import { closeSync, existsSync, mkdirSync, openSync, utimesSync, writeSync } from "fs";
+import { closeSync, existsSync, mkdirSync, openSync, utimesSync } from "fs";
 
 type ICallbackFn = () => void;
 
@@ -15,9 +15,7 @@ export const createFile = (file: string, callback?: ICallbackFn): void => {
   try {
     utimesSync(file, time, time);
   } catch (e) {
-    const message = `This log file was created on ${time}\n${"=".repeat(100)}\n`;
     const fd = openSync(file, "a");
-    writeSync(fd, message);
     closeSync(fd);
   }
   isDevEnv && console.log(`Initialized file: ${file}`);
